8d2a3b3b01f737893e5d17c45a12da49c0fc49bf
Maëlys Bras de fer <m.brasdefer@yeswehack.com>
2021-04-25 02:18:57 +0200

    Add Apollo auth middleware

--- dockers/manager/front/src/apollo/apollo-client-hooks.js
+++ dockers/manager/front/src/apollo/apollo-client-hooks.js
@@ -1,6 +1,17 @@
-export async function apolloClientBeforeCreate (/* { apolloClientConfigObj, app, router, store, ssrContext, urlPath, redirect } */) {
-  // if needed you can modify here the config object used for apollo client
-  // instantiation
+import { ApolloLink, from } from "apollo-link";
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      Authorization: "Bearer " + localStorage.getItem("token")
+    }
+  }));
+  return forward(operation);
+});
+
+export async function apolloClientBeforeCreate({ apolloClientConfigObj }) {
+  apolloClientConfigObj.link = from([authLink, apolloClientConfigObj.link]);
 }
 
 export async function apolloClientAfterCreate (/* { apolloClient, app, router, store, ssrContext, urlPath, redirect } */) {
