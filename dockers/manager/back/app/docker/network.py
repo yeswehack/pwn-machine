a46b9809b1a37cf6fc6eca1ebe86c6ea685045b0
Maëlys Bras de fer <m.brasdefer@yeswehack.com>
2021-05-13 21:37:47 +0200

    Add useful fields in Docker Network GraphQL type

--- dockers/manager/back/app/docker/network.py
+++ dockers/manager/back/app/docker/network.py
@@ -23,3 +23,23 @@ async def resolve_network_created(network, _):
 @DockerNetwork.field("ipv6")
 async def resolve_network_ipv6(network, _):
     return network.attrs["EnableIPv6"]
+
+
+@DockerNetwork.field("driver")
+async def resolve_network_driver(network, _):
+    return network.attrs["Driver"]
+
+
+@DockerNetwork.field("internal")
+async def resolve_network_internal(network, _):
+    return network.attrs["Internal"]
+
+
+@DockerNetwork.field("subnet")
+async def resolve_network_subnet(network, _):
+    return (network.attrs["IPAM"]["Config"] or [{}])[0].get("Subnet")
+
+
+@DockerNetwork.field("gateway")
+async def resolve_network_gateway(network, _):
+    return (network.attrs["IPAM"]["Config"] or [{}])[0].get("Gateway")
