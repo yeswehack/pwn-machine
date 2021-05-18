406294ad2f3ea38c2484415d483fd4fcfaa101b0
BitK <bitk@yeswehack.com>
2021-05-18 19:16:22 +0200

    Complety fix tab-page transition

--- dockers/manager/front/src/components/TabPage.vue
+++ dockers/manager/front/src/components/TabPage.vue
@@ -1,71 +1,42 @@
 <template>
-  <q-page padding class="page">
+  <q-page padding class="page" ref="page">
     <q-tabs
       dense
       rounded
       inline-label
       shadow
+      ref="tabs"
       class="text-white bg-dark rounded-borders tabs q-pr-sm"
       active-color="primary"
       align="left"
       narrow-indicator
-      v-mutation="hookNavigation"
     >
       <slot name="default"></slot>
     </q-tabs>
     <div
       class="row justify-center bg-dark q-pa-md page-content rounded-borders"
     >
-      <transition @before-enter="beforeEnter" @before-leave="beforeLeave">
-        <router-view class="animated" :key="$route.path" />
-      </transition>
+      <div class="col relative">
+        <transition name="slide">
+          <router-view :key="$route.path" class="view"></router-view>
+        </transition>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      oldIdx: null,
-      oldSlots: null
-    };
-  },
-  mounted() {
-    this.hookNavigation();
-  },
-  methods: {
-    hookNavigation() {
-      if (this.oldSlots == this.$slots) return;
-      for (const [idx, { elm }] of this.$slots.default.entries()) {
-        if (elm.getAttribute("aria-current") == "page") {
-          this.oldIdx = idx;
-        }
-        elm.setAttribute("aria-idx", idx);
-      }
-      this.oldSlots = this.$slots;
-    },
-    beforeEnter(el){
-      const linkEl = document.activeElement.parentElement;
-      const newIdx = linkEl.getAttribute("aria-idx");
-      const slideOut = newIdx < this.oldIdx ? "slideInLeft" : "slideInRight";
-      el.classList.add(slideOut);
-    },
-    beforeLeave(el) {
-      const linkEl = document.activeElement.parentElement;
-      const newIdx = linkEl.getAttribute("aria-idx");
-      const slideOut = newIdx < this.oldIdx ? "slideOutRight" : "slideOutLeft";
-      this.oldIdx = newIdx;
-      el.classList.add(slideOut);
+  watch: {
+    $route(to, from) {
+      const links = Array.from(this.$refs.tabs.$el.querySelectorAll(".q-tab"));
+      const findPos = route =>
+        links.findIndex(l => l.attributes.href.value == route.path);
 
-      const { width, height } = el.getBoundingClientRect();
-      Object.assign(el.style, {
-        width: `${width}px`,
-        height: `${height}px`,
-        position: "absolute",
-        top: "76px",
-        left: "16px"
-      });
+      const oldIdx = findPos(from);
+      const newIdx = findPos(to);
+      const direction = newIdx < oldIdx ? 1 : -1;
+      this.$refs.page.$el.style.setProperty("--direction", direction);
     }
   }
 };
@@ -75,32 +46,21 @@ export default {
   border-top-left-radius: 0;
   border-top-right-radius: 0;
   overflow-x: hidden;
-
-  // Hacky way to hide children transition overflow
-  &::before,
-  &::after {
-    content: "";
-    width: 24px;
-    position: absolute;
-    top: 0;
-    height: 100%;
-
-    background: #121212;
-    z-index: 100;
-  }
-  &::before {
-    left: 0px;
-  }
-  &::after {
-    right: 0px;
-  }
 }
 .page .tabs {
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
   text-decoration: none;
 }
+.view {
+  top: 0;
+}
+.relative {
+  position: relative;
+  overflow: hidden;
+}
 .page {
+  --direction: 1;
   display: grid;
   width: 100%;
   grid-template-columns: 100%;
@@ -109,6 +69,25 @@ export default {
   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2), 0 2px 2px rgba(0, 0, 0, 0.14),
     0 3px 1px -2px rgba(0, 0, 0, 0.12);
   overflow-x: hidden;
+}
+.slide-enter-active,
+.slide-leave-active {
+  position: absolute;
+  transition: 300ms;
+}
+
+.slide-enter {
+  transform: translateX(calc(100% * -1 * var(--direction)));
+}
+.slide-enter-to {
+  transform: translateX(0);
+}
+
+.slide-leave {
+  transform: translateX(0);
+}
+.slide-leave-to {
+  transform: translateX(calc(100% * var(--direction)));
 }
 </style>
 <style>
