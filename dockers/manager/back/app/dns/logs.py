de4ce2f1a5e3433088e48624590e69d345e49359
BitK <bitk@yeswehack.com>
2021-05-18 11:36:30 +0200

    add mapping and wait for es to start

--- dockers/manager/back/app/dns/logs.py
+++ dockers/manager/back/app/dns/logs.py
@@ -16,17 +16,54 @@ def with_dns_redis(f):
 DnsLog = createType("DnsLog")
 
 
+def log_from_es_to_gql(log):
+    entry = {**log["_source"]}
+    entry["nodeId"] = log["_id"]
+    if log["_source"]["return_code"] != "NOERROR":
+        entry["error"] = log["_source"]["return_code"]
+    return entry
+
+
 @registerQuery("dnsLogs")
 async def query_dns_logs(*_, filter={}, cursor={}):
-    r = await es.search(index="powerdns-logs", sort="date:desc")
-    logs = []
-    for hit in r["hits"]["hits"]:
-        entry = hit["_source"]
-        entry["nodeId"] = hit["_id"]
-        if hit["_source"]["return_code"] != "NOERROR":
-            entry["error"] = hit["_source"]["return_code"]
-        logs.append(entry)
-    return logs
+    from_ = max(cursor["from"], 0)
+    size = max(min(cursor["size"], 100), 0)
+
+    query_filter = "*" + filter.get("query", "") + "*"
+    type_filter = "*" + filter.get("type", "") + "*"
+
+    body = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"wildcard": {"query": query_filter}},
+                    {"wildcard": {"type": type_filter}},
+                ],
+            },
+        }
+    }
+
+    r = await es.search(
+        index="powerdns-logs",
+        sort="date:desc",
+        body=body,
+        from_=from_,
+        size=size,
+    )
+    hits = r["hits"]
+    total = hits["total"]["value"]
+    next_from = from_ + len(hits["hits"])
+    response = {
+        "total": hits["total"]["value"],
+        "result": [log_from_es_to_gql(h) for h in hits["hits"]],
+        "next": None,
+        "prev": None,
+    }
+    if next_from < total:
+        response["next"] = {"from": next_from, "size": size}
+    if from_ > 0:
+        response["prev"] = {"from": max(from_ - size, 0), "size": size}
+    return response
 
 
 @registerSubscription("dnsLogs")
