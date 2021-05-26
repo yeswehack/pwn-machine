1f40566d316ee02b2eb4570c894a1130fe803490
Maëlys Bras de fer <m.brasdefer@yeswehack.com>
2021-05-26 14:21:48 +0200

    Change Auth back API

--- dockers/manager/back/app/auth/auth.py
+++ dockers/manager/back/app/auth/auth.py
@@ -22,17 +22,31 @@ db = Database()
 
 
 @auth_query("authNeedsSetup")
-async def resolve_auth_needs_setup(*_):
+async def resolve_needs_setup(*_):
     if await db.password_hash is None:
-        return {"step": "PASSWORD"}
+        return "PASSWORD"
     if await db.totp_client is None:
-        secret = pyotp.random_base32()
-        uri = pyotp.totp.utils.build_uri(secret, ISSUER)
-        return {"step": "TOTP", "totpUri": uri}
+        return "TOTP"
 
 
-@auth_mutation("login")
-async def resolve_login(*_, password, totp, expire=None):
+@auth_query("authTotpUri")
+async def resolve_totp_uri(*_):
+    secret = pyotp.random_base32()
+    return pyotp.totp.utils.build_uri(secret, ISSUER)
+
+
+async def make_jwt_auth(expire=None):
+    now = int(time.time())
+    payload = {"iss": ISSUER, "iat": now}
+    if expire is not None:
+        payload["exp"] = now + expire
+
+    token = jwt.encode(payload, await db.jwt_secret)
+    return {"token": token, "expire": payload.get("exp")}
+
+
+@auth_mutation("createAuthToken")
+async def resolve_create_token(*_, password, totp, expire=None):
     if AUTH_DISABLED is False:
         try:
             hasher.verify(await db.password_hash, password)
@@ -41,25 +55,32 @@ async def resolve_login(*_, password, totp, expire=None):
         if not (await db.totp_client).verify(totp):
             return False
 
-    now = int(time.time())
-    payload = {"iss": ISSUER, "iat": now}
-    if expire is not None:
-        payload["exp"] = now + expire
-    token = jwt.encode(payload, await db.jwt_secret)
-    return {"token": token, "expire": payload.get("exp")}
+    return make_jwt_auth(expire)
 
 
-@auth_mutation("setPassword")
+@auth_mutation("refreshAuthToken")
+async def resolve_resfresh_token(*_, token, expire=None):
+    if AUTH_DISABLED is False:
+        try:
+            jwt.decode(token, await db.jwt_secret, ["HS256"], issuer=ISSUER)
+        except jwt.exceptions.InvalidTokenError:
+            return None
+
+    return make_jwt_auth(expire)
+
+
+@auth_mutation("updateAuthPassword")
 async def resolve_set_password(*_, password):
     await db.save_password_hash(hasher.hash(password))
     return True
 
 
-@auth_mutation("setTotp")
+@auth_mutation("updateAuthTotp")
 async def resolve_set_totp(*_, uri, totp):
     totp_client = pyotp.parse_uri(uri)
     if not totp_client.verify(totp):
         return False
+
     await db.save_totp_client(totp_client, uri)
     return True
 
@@ -78,7 +99,7 @@ async def auth_middleware(resolver, obj, info, **args):
             headers = info.context["request"].headers
             token = headers["Authorization"].split()[-1]
             jwt.decode(token, await db.jwt_secret, ["HS256"], issuer=ISSUER)
-        except:
+        except jwt.exceptions.InvalidTokenError:
             raise Exception("Unauthorized")
 
     res = resolver(obj, info, **args)
