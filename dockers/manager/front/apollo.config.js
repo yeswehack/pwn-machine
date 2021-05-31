8685aa2d01f14628a678968516e59c51ffeda9ab
BitK <bitk@yeswehack.com>
2021-05-31 16:56:21 +0200

    fix apollo config

--- dockers/manager/front/apollo.config.js
+++ dockers/manager/front/apollo.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   client: {
     service: {
-      name: "my-app",
+      name: "pwn-machine",
       // URL to the GraphQL API
-      url: "http://127.0.0.1:8000/api/"
+      url: "http://127.0.0.1:8000/api"
     },
     // Files processed by the extension
     includes: ["src/**/*.vue", "src/**/*.js", "src/**/*.graphql"]
